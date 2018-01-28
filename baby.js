ab390c9ef771d8209889c972e83d3328f27ef752
LDQ-first <178848890@qq.com>
2018-01-28 13:11:02 +0800

    修改baby.ts.生成baby.d.ts

--- baby.js
+++ baby.js
@@ -1,7 +1,17 @@
+"use strict";
+exports.__esModule = true;
 var Baby = /** @class */ (function () {
-    function Baby() {
+    function Baby(name) {
+        this._name = name;
         console.log('小宝贝正在哭泣');
     }
+    Baby.smile = function () {
+        console.log('(｡◕ˇ∀ˇ◕)');
+    };
+    Baby.prototype.getBabyName = function () {
+        return this._name;
+    };
     return Baby;
 }());
-var baby = new Baby();
+exports.Baby = Baby;
+exports.baby = new Baby('Mike');
--- baby.ts
+++ baby.ts
@@ -1,7 +1,17 @@
-class Baby {
-  constructor() {
+export class Baby {
+  private _name: string
+  constructor(name: string) {
+    this._name = name;
     console.log('小宝贝正在哭泣')
+  }
+
+  static smile() {
+    console.log('(｡◕ˇ∀ˇ◕)')
+  }
+
+  public getBabyName(): string {
+    return this._name
   }
 }
 
-let baby = new Baby()
+export let baby = new Baby('Mike')
